49ea4afde6e8acd9a4ad86ec2896f73477e46cf4
bbialke <bradbialke05@gmail.com>
2020-05-08 14:39:17 -0700

    Added files and modules

---
+++ functions/quickDraw.js
@@ -0,0 +1,7 @@
+const random = require('random')
+async function quickDraw(guess[]) {
+  console.log(guess);
+}
+
+
+module.exports = quickDraw;
---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  quickDraw: require('./functions/quickDraw'),
+};
