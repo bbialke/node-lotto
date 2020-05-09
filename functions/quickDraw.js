155ebe02032d584adcbc82ae1108b5f4c0cf9361
bbialke <bradbialke05@gmail.com>
2020-05-08 17:38:57 -0700

    Updated quickDraw return to JSON

--- functions/quickDraw.js
+++ functions/quickDraw.js
@@ -1,23 +1,37 @@
 const random = require('random')
-function quickDraw(guess) {
+async function quickDraw(guess) {
 
   //Before even checking the guess, get the winning numbers
   let winningNumbers=[];
   let i = 0;
   while(i <= 6){
-    winningNumbers.push(random.int(min = 1, max = 99));
+    winningNumbers.push(random.int(min = 1, max = 15));
     i++;
   }
 
   //Check if guess is a valid array
   if(!Array.isArray(guess)) {
-    throw new Error ('Guess must be formatted as an array');
+    throw new Error ('quickDraw expects guess to be an array');
   }
   //Check length of guess
   if(guess.length != 6){
-    throw new Error ('quickDraw expects an array of 6 numbers between 1 and 99');
+    throw new Error ('quickDraw expects guess to be an array of 6 numbers between 1 and 99');
   }
-  console.log(`Guess was ${guess}. Winning numbers were ${winningNumbers}`);
+  let matches=[];
+  guess.forEach((value) => {
+    if(winningNumbers.indexOf(value) !== -1){
+      matches.push(value);
+    }
+  });
+  if(matches.length == 0){
+    matches = 'none';
+  }
+  var output = {
+    winningNumbers: winningNumbers,
+    guess: guess,
+    matches: matches
+  }
+  return output;
 }
 
 
